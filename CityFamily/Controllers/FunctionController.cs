696be791004c6c5872cef68d3c02dacc6a343a88
leroyu <1198470013@qq.com>
2016-03-22 15:14:10 +0800

    添加获取功能定位下360展示列表信息接口

--- CityFamily/Controllers/FunctionController.cs
+++ CityFamily/Controllers/FunctionController.cs
@@ -61,5 +61,41 @@ namespace CityFamily.Controllers
                 db.SaveChanges();
             }
         }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns>
+        /// {
+        ///     "material":"http://home.vrcan.com/Home/Iv/21730",
+        ///     "menTing":"http://119.188.113.104:8081/SpaceCate/MenTing/MenTing.html",
+        ///     "keTing":"http://119.188.113.104:8081/SpaceCate/KeTing/KeTing.html",
+        ///     "canTing":"http://119.188.113.104:8081/SpaceCate/CanTing/CanTing.html",
+        ///     "zhuWo":"http://119.188.113.104:8081/SpaceCate/ZhuRenFang/ZhuRenFang.html",
+        ///     "laoRen":"http://119.188.113.104:8081/SpaceCate/LaoRenFang/LaoRenFang.html",
+        ///     "erTong":"http://119.188.113.104:8081/SpaceCate/ErTongFang/ErTongFang.html",
+        ///     "chuFang":"http://119.188.113.104:8081/SpaceCate/ChuFang/ChuFang.html",
+        ///     "weiSheng":"http://119.188.113.104:8081/SpaceCate/WeiShengJian/WeiShengJian.html",
+        ///     "chuWu":"http://119.188.113.104:8081/Spacecate/YiMaoJian/YiMaoJian.html"
+        ///     "duoGongNeng":"http://119.188.113.104:8081/Spacecate/DuoGongNeng/DuoGongNeng.html"
+        /// }
+        /// </returns>
+        [HttpPost]
+        public ActionResult GetSpaceLink()
+        {
+            string material = db.SpaceCate.Where(item => item.Category == 23).FirstOrDefault().SpacePics;
+            string menTing = db.SpaceCate.Where(item => item.Category == 1).FirstOrDefault().SpacePics;
+            string keTing = db.SpaceCate.Where(item => item.Category == 10).FirstOrDefault().SpacePics;
+            string canTing = db.SpaceCate.Where(item => item.Category == 11).FirstOrDefault().SpacePics;
+            string zhuWo = db.SpaceCate.Where(item => item.Category == 3).FirstOrDefault().SpacePics;
+            string laoRen = db.SpaceCate.Where(item => item.Category == 4).FirstOrDefault().SpacePics;
+            string erTong = db.SpaceCate.Where(item => item.Category == 5).FirstOrDefault().SpacePics;
+            string chuFang = db.SpaceCate.Where(item => item.Category == 6).FirstOrDefault().SpacePics;
+            string weiSheng = db.SpaceCate.Where(item => item.Category == 7).FirstOrDefault().SpacePics;
+            string chuWu = db.SpaceCate.Where(item => item.Category == 8).FirstOrDefault().SpacePics;
+            string duoGongNeng = db.SpaceCate.Where(item => item.Category == 9).FirstOrDefault().SpacePics;
+
+            return Json(new { material = material, menTing = menTing, keTing = keTing, canTing = canTing, zhuWo = zhuWo, laoRen = laoRen, erTong = erTong, chuFang = chuFang, weiSheng = weiSheng, chuWu = chuWu, duoGongNeng = duoGongNeng }, JsonRequestBehavior.AllowGet);
+
+        }
     }
 }
