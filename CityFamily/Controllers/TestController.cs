c21b9b629ea9c5d060e88c779de17475b00db930
leroyu <1198470013@qq.com>
2016-04-27 11:14:09 +0800

    测试成功数据库更新缓存

--- CityFamily/Controllers/TestController.cs
+++ CityFamily/Controllers/TestController.cs
@@ -13,5 +13,13 @@ namespace CityFamily.Controllers
         {
             return View();
         }
+        [OutputCache(CacheProfile = "SqlDependencyCache")]
+        public ActionResult Test()
+        {
+            System.Web.Caching.SqlCacheDependencyAdmin.EnableNotifications(System.Configuration.ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString);//通知哪个数据库,name是该数据库链接字符串的名字
+            System.Web.Caching.SqlCacheDependencyAdmin.EnableTableForNotifications(System.Configuration.ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString, "Users");//通知启用哪个表，Player是表名
+            string time = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+            return Content(time);
+        }
     }
 }
